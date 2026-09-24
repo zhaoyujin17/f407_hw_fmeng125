e8387c0d64fab092b8206d35b86f50ef9da6a862
17P-Holmes <yujinzhao@17P-HolmesdeMacBook-Pro.local>
2026-09-24 21:57:37 +0800

    first commit

---
+++ build/Debug/CMakeFiles/4.4.3/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Darwin-25.6.0")
+set(CMAKE_HOST_SYSTEM_NAME "Darwin")
+set(CMAKE_HOST_SYSTEM_VERSION "25.6.0")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "arm64")
+
+include("/Users/yujinzhao/electric/gpio/shiyan/cmake/gcc-arm-none-eabi.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "arm")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
